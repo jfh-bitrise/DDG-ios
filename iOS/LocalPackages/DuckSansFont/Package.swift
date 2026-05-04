c92eead6d12b7cb09604996116af7fabced3bda1
Julia Fodor-Horvath <99810578+jfh-bitrise@users.noreply.github.com>
2026-05-04 13:04:37 +0200

    move DuckSansFont local

---
+++ iOS/LocalPackages/DuckSansFont/Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "DuckSansFont",
+    products: [
+        .library(name: "DuckSansFont", targets: ["DuckSansFont"]),
+    ],
+    targets: [
+        .target(name: "DuckSansFont"),
+    ]
+)
---
+++ iOS/LocalPackages/DuckSansFont/Sources/DuckSansFont/DuckSansFont.swift
@@ -0,0 +1,3 @@
+public enum DuckSansFont {
+    public static func registerFonts() {}
+}
